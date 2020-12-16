ea7493d18c53560178135b6936fe7e932188c8f0
uyeonH <yooyeon0508@gmail.com>
2020-12-16 16:35:43 +0900

    lombok getter setter

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
@@ -1,8 +1,14 @@
 package com.fastcampus.javaallinone.project3.mycontact.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 public class Person {
@@ -11,7 +17,42 @@ public class Person {
     @GeneratedValue
     private Long id;
 
+    @Getter
+    @Setter
     private String name;
 
+    @Getter
+    @Setter
+    private String hobby;
+
+    @Getter
+    @Setter
+    private String bloodType;
+
+    @Getter
+    @Setter
+    private String address;
+
+    @Getter
+    @Setter
+    private LocalDate birthday;
+
+    @Getter
+    @Setter
+    private String job;
+
+    @Getter
+    @Setter
     private int age;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name=\'" + name + '\'' +
+                ", age=" + age +
+                "}";
+
+    }
+
+
 }
--- src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -14,10 +17,20 @@ class PersonRepositoryTest {
     private PersonRepository personRepository;
 
     @Test
-    void crud(){
-        Person person=new Person();
+    void crud() {
+        Person person = new Person();
+        person.setName("yooyeon");
+        person.setAge(100);
+        person.setBloodType("A");
+
         personRepository.save(person);
-        System.out.println(personRepository.findAll());
+//        System.out.println(personRepository.findAll());
+
+        List<Person> people = personRepository.findAll();
+        assertThat(people.size()).isEqualTo(1);
+        assertThat(people.get(0).getName()).isEqualTo("yooyeon");
+        assertThat(people.get(0).getAge()).isEqualTo(100);
+        assertThat(people.get(0).getBloodType()).isEqualTo("A");
     }
 
 
